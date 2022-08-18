98ae4f7936714ff64cf0e557cb2dbaa8afe23ba2
unknown <perceptionarmy@icloud.com>
2022-08-19 00:42:07 +0300

    header

---
+++ scripts/headerScripts.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    let burger_button_open = document.getElementById('burger_button_open')
+    let burger_button_close = document.getElementById('burger_button_close')
+    let burgermenu = document.getElementById('burgermenu')
+
+    burger_button_open.onclick = function () {
+        burgermenu.style.display = 'block'
+    }
+
+    burger_button_close.onclick = function () {
+        burgermenu.style.display = 'none'
+    }
+}
